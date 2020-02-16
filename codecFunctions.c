5b2e19ba4f07026c718200df97f447232f06fe13
Nouu <noelarrieulacoste@yahoo.fr>
2020-02-16 22:35:22 +0100

    readKey

--- codecFunctions.c
+++ codecFunctions.c
@@ -3,3 +3,19 @@
 //
 
 #include "codecFunctions.h"
+#include "gtkFunctions.h"
+#include "functions.h"
+
+int readKey(const char *path) {
+    char c1[9], c2[9], c3[9], c4[9];
+    FILE *fp = fopen(path, "rb");
+    if (fp == NULL) {
+        updateStatus("Cannot open key file !");
+        return 1;
+    }
+    if (fscanf(fp, "G4C=[%[01] %[01] %[01] %[01]]", c1, c2, c3, c4) != 4) {
+        updateStatus("Incorrect key file !");
+        return 1;
+    }
+    return 0;
+}
--- codecFunctions.h
+++ codecFunctions.h
@@ -4,5 +4,9 @@
 
 #ifndef CODECC_CODECFUNCTIONS_H
 #define CODECC_CODECFUNCTIONS_H
+char key[4];
+
+
+int readKey(const char *path);
 
 #endif //CODECC_CODECFUNCTIONS_H
